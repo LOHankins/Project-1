2362c1967525179d930c78a84b93e47a077ce113
Louis <LOHankins@gmail.com>
2016-03-30 12:59:09 -0400

    Completed basics of assignment. need to add instructions

--- trivia.js
+++ trivia.js
@@ -5,6 +5,7 @@ var askForNameVar="OK Player, What is your name";
 var entry=document.getElementById("fromPlayer");
 var nameOfPlayer="";
 var startButton=document.getElementById("start");
+var stopButton=document.getElementById("stop");
 var startGame ="Ok " + nameOfPlayer + ", Select the Politics category and Press the Start button when you are ready!";
 var i = 0;
 var firstchoice = document.querySelector("#trythis1p");
@@ -15,10 +16,17 @@ var checkBox1 = document.getElementById("cbox1");
 var checkBox2 = document.getElementById("cbox2");
 var checkBox3 = document.getElementById("cbox3");
 var checkBox4 = document.getElementById("cbox4");
-var congratulations = "Congratulations on the correct answer!";
+var congratulations = "Congratulations on the correct answer!  When you are ready for the next question, hit the start button.";
+var admonishment = "That was an incorrect answer. When you are ready for the next question, hit the start button.";
+var endOfGameText = "You have completed all of the questions for this category.  Your results are displayed on the scoreboard.  If you want to continue, pick another category and keep going.";
+var stopGameText = "You have elected to stop play.  Your results are displayed on the scoreboard.  If you want to continue, pick another category and keep going.";
+var messageContent = "";
+var currentScore = 0;
+var hiScore = 0;
+var questionsAnswered = 0;
+var answeredCorrectly = 0;
 
 var politics = [{question:"Who is the POTUS?", correct:"Barack Obama", bad1:"George W. Bush", bad2:"Hillary Clinton", bad3:"Donald Trump"}, {question:"Who is the Vice-President?", correct:"Joe Biden", bad1:"Richard Daley", bad2:"Rayful Edmunds", bad3:"Marion Barry"}, {question:"In 2012, who did Barack Obama beat to win a second term?", correct:"Mitt Romney", bad1:"Bill Oreilly", bad2:"Sarah Palin", bad3:"John Boehner"}, {question:"Who was the Vice President for George W. Bush?", correct:"Dick Cheney", bad1:"George Jefferson", bad2:"Sarah Day O'Connor", bad3:"Vito Corleone"}, {question:"Who was the Vice President for Ronald Reagan?", correct:"George H W Bush", bad1:"George W. Bush", bad2:"Jeb Bush", bad3:"Bill Clinton"}];
-console.log(startButton);
 
 function Validateentry (playerMessage) {
 	var messageChoices = ["y", "yes", "Yes", "Y"];
@@ -44,7 +52,10 @@ function askForName () {
 function clickCheckBox() {
 	console.log("clickCheckBox");
 	var cboxChoices = [checkBox2.checked, checkBox3.checked, checkBox4.checked];
+	questionsAnswered += 1;
+
 	if (checkBox1.checked === true) {
+		answeredCorrectly += 1;
 		correctAnswer();
 	}
 	else if (cboxChoices.includes(true)) {
@@ -55,11 +66,23 @@ function clickCheckBox() {
 	}
 }
 
+function clearCheckboxes() {
+	console.log("clearCheckboxes");
+	checkBox1.checked = false;
+	checkBox2.checked = false;
+	checkBox3.checked = false;
+	checkBox4.checked = false;
+}
+
 function correctAnswer() {
 	console.log("correctAswer");
 	var points = 0;
 	sendCongrats ();
 	points = 100;
+	currentScore = currentScore + points;
+	if (currentScore > hiScore) {
+		hiScore = currentScore;
+	}
 	updateScoreboard();
 
 }
@@ -71,11 +94,31 @@ function sendCongrats() {
 
 function updateScoreboard() {
 	console.log("updateScoreboard");
+	document.getElementById("hiScore").textContent = hiScore;
+	document.getElementById("currentScore").textContent = currentScore;
+}
+
+function wrongAnswer() {
+	console.log("wrongAnswer");
+	var points = 0;
+	sendAdmonishment();
+	currentScore = currentScore + points;
+	if (currentScore > hiScore) {
+		hiScore = currentScore;
+	}
+	updateScoreboard();
+}
+
+function sendAdmonishment() {
+	console.log("sendAdmonishment");
+	document.getElementById("messages").textContent = admonishment;
+
 }
 
 function clickStartButton() {
 	console.log("clickStartButton");
 	var category = getCategory();
+	messageContent = "";
 	switch (category) {
 		case "2":
 		// politics
@@ -91,6 +134,17 @@ function clickStartButton() {
 		default:
 			console.log("no category");
 	}
+
+	clearCheckboxes();
+	document.getElementById("messages").textContent = messageContent;
+
+}
+
+function clickStopButton() {
+	console.log("clickStopButton");
+	endOfGame();
+	clearCheckboxes();
+	document.getElementById("messages").textContent = stopGameText;
 }
 
 function loadPQuestion(i) {
@@ -106,15 +160,26 @@ function loadPQuestion(i) {
 
 function loadPolitics() {
 	console.log("loadPolitics");
-	loadPQuestion(i);
-	i += 1;
+	if (i > (politics.length - 1)) {
+		endOfGame();
+	}
+	else {
+		loadPQuestion(i);
+		i += 1;
+	}
+
+}
+
+function endOfGame() {
+	console.log("endOfGame");
+	messageContent = endOfGameText;
+	document.getElementById("endofgametext").textContent = "You answered " + answeredCorrectly + " out of " + questionsAnswered + " questions";
 
 }
 
 function getCategory() {
 	console.log("getCategory");
 	var value = document.getElementById("category").value;
-	console.log(typeof(value));
 	return value;
 
 }
@@ -141,6 +206,7 @@ entry.addEventListener( "keypress", function (event) {
 });
 
 startButton.addEventListener("click", clickStartButton);
+stopButton.addEventListener("click", clickStopButton);
 checkBox4.addEventListener("click", clickCheckBox);
 checkBox3.addEventListener("click", clickCheckBox);
 checkBox2.addEventListener("click", clickCheckBox);
